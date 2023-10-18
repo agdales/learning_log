ccfa53662937dfb8bd64e08951a0fdca098934df
agdales <agdales@yahoo.co.uk>
2023-10-18 21:26:48 +0100

    comment added

--- learning_log/urls.py
+++ learning_log/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+# url patterns for learning log
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('learning_logs.urls')),
